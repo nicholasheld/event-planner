5be19f45ee3b526195ba573712a0d243666f35d1
nicholas <nicholaspheld@gmail.com>
2021-03-18 15:37:01 -0500

    added JS file with working moment js and edited the index to display calendar blocks

---
+++ script.js
@@ -0,0 +1,20 @@
+// save reference to important DOM elements
+var timeDisplayEl = $('#currentDay');
+var eventDisplayEl = $('#event-display');
+var projectModalEl = $('#project-modal');
+var projectFormEl = $('#project-form');
+var projectNameInputEl = $('#project-name-input');
+var projectTypeInputEl = $('#project-type-input');
+var hourlyRateInputEl = $('#hourly-rate-input');
+var dueDateInputEl = $('#due-date-input');
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+setInterval(displayTime, 1000);
+
+function printEvent(event) {
+    var eventDisplayEl = $('<tr>');
